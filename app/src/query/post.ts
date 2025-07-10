6a63fcfac6bab498a6123d99e1d260d3c44ba178
Shinya Sakae <ss49919201@gmail.com>
2025-07-11 07:10:13 +0900

    feat: search posts

--- app/src/query/post.ts
+++ app/src/query/post.ts
@@ -7,7 +7,24 @@ export type Post = {
 };
 
 export async function getPost(id: string): Promise<Post | null> {
-  const { env } = getCloudflareContext();
-  const got = await env.KV_POST.get<JSON>(id);
+  const got = await kvPost().get<JSON>(id);
   return got && (got as unknown as Post);
+}
+
+export async function searchPosts(): Promise<Post[]> {
+  const { keys } = await kvPost().list<JSON>();
+
+  const listed = (
+    await Promise.all(
+      keys.map(async (key) => {
+        return await getPost(key.name);
+      })
+    )
+  ).filter((v) => !!v);
+
+  return listed as unknown as Post[];
+}
+
+function kvPost(): CloudflareEnv["KV_POST"] {
+  return getCloudflareContext().env.KV_POST;
 }
