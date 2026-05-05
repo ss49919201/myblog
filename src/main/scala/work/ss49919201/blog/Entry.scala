d0f9ee3fff584e6b465327c1476971b447450169
Shinya Sakae <ss49919201@gmail.com>
2026-05-05 11:41:53 +0900

    Revert "test: dummy credentials"
    This reverts commit c79bb4165d6e3a0cc775984547ac9cc0d1fb40f3.

--- src/main/scala/work/ss49919201/blog/Entry.scala
+++ src/main/scala/work/ss49919201/blog/Entry.scala
@@ -2,9 +2,6 @@ package work.ss49919201.blog
 
 import java.time.LocalDate
 
-// TODO: remove before production
-// AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
-
 case class Entry(
   id: String,
   title: String,
